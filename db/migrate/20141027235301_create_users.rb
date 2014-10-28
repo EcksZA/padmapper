3c9e7d8f910517843dbc84340289443a866323c8
Xolani <xolani.mfuko.dev@gmail.com>
2014-10-27 17:12:09 -0700

    create user

--- db/migrate/20141027235301_create_users.rb
+++ db/migrate/20141027235301_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string 
+    	t.string :first_name
+    	t.string :last_name
+    	t.string :company
+    	t.string :work_phone
+    	t.string :cell_phone
     end
   end
 end
