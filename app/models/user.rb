a3e0a04d8800170f3832a3b7b8d314bef1a2587e
Xolani <xolani.mfuko.dev@gmail.com>
2014-10-27 17:22:29 -0700

    validates presence of user last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
 	validates :first_name, presence: true
+	validates :last_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe User do
 	it { should validate_presence_of :first_name }
+	it { should validate_presence_of :last_name }
 end
