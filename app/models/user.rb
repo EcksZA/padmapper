7ac14a436bea084b8f67f9feec09847b17fda125
Xolani <xolani.mfuko.dev@gmail.com>
2014-10-27 17:25:02 -0700

    validates presence of user work_phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 	validates :company, presence: true
+	validates :work_phone, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,5 @@ describe User do
 	it { should validate_presence_of :first_name }
 	it { should validate_presence_of :last_name }
 	it { should validate_presence_of :company }
+	it { should validate_presence_of :work_phone }
 end
