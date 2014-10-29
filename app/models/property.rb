7b01bbaf9b0868a397cd83b13e909b9f9ef0cd95
Xolani <xolani.mfuko.dev@gmail.com>
2014-10-28 17:49:29 -0700

    validates the presence of price in properties model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ActiveRecord::Base
 	validates :address, presence: true
-	
+	validates :price, presence: true
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Property do
 	it { should validate_presence_of :address }
+	it { should validate_presence_of :price }
 end
