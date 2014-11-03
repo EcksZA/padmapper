740ee47aad916092b276a06d3c0d8179b55d8d5e
Xolani <xolani.mfuko.dev@gmail.com>
2014-11-03 13:24:10 -0800

    edit users controller to allow for authentication sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:notice] = 'Your New Account Has Been Created'
 			redirect_to user_path(@user)
 		else
