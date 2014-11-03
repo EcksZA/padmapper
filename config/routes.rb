118c54cfd6c22a8722809298ae62660021da387c
Xolani <xolani.mfuko.dev@gmail.com>
2014-11-03 13:24:50 -0800

    edit routes to make shorter urls for the authentication process

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+	get 'signup', to: 'users#new', as: 'signup'
+	get 'login', to: 'sessions#new', as: 'login'
+	get 'logout', to: 'sessions#destroy', as: 'logout'
+
 	root to: 'properties#index'
 	
 	resources :properties, only: [:index]
+	resources :sessions, only: [:new, :create, :destroy]
 
   resources :users do
   	resources :properties, except: [:index]
